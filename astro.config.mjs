90660e849384e52d2c9cd49109508c12befdd092
Ahamed Bilal <ahamedbilalazmy@gmail.com>
2023-10-09 19:40:31 +0530

    Complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astro-101.com',
+  site: 'https://astro-101.com',
+  integrations: [preact()]
 });
